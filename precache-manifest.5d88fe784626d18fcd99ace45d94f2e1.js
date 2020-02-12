9cac2b2669a75a0f04a2e3433e6cdbd76f75e204
PeCera <pacoelayudante@gmail.com>
2020-02-12 01:50:10 -0300

    Updates

---
+++ precache-manifest.5d88fe784626d18fcd99ace45d94f2e1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01130768ad33b033f403c1f5ec2eee7c",
+    "url": "/vigilantes-de-belona/index.html"
+  },
+  {
+    "revision": "00b9f46307f277bf9a39",
+    "url": "/vigilantes-de-belona/static/css/main.44a1b6d1.chunk.css"
+  },
+  {
+    "revision": "05ff2057331fc4bb808e",
+    "url": "/vigilantes-de-belona/static/js/2.d219b545.chunk.js"
+  },
+  {
+    "revision": "41a40462cc6b0ccf2219289c9a69694f",
+    "url": "/vigilantes-de-belona/static/js/2.d219b545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b9f46307f277bf9a39",
+    "url": "/vigilantes-de-belona/static/js/main.0f667946.chunk.js"
+  },
+  {
+    "revision": "e645d2e38c57259bf434",
+    "url": "/vigilantes-de-belona/static/js/runtime-main.51153c05.js"
+  },
+  {
+    "revision": "5cba0a3961b2f343ba009d371e213846",
+    "url": "/vigilantes-de-belona/static/media/char1.5cba0a39.jpg"
+  },
+  {
+    "revision": "a6e831427426d98fc98b61955b7a691b",
+    "url": "/vigilantes-de-belona/static/media/goblin.a6e83142.jpg"
+  },
+  {
+    "revision": "91704a6dc44e2dad964272bfce26ae34",
+    "url": "/vigilantes-de-belona/static/media/noun_badge_1266359.91704a6d.png"
+  },
+  {
+    "revision": "543b31a7b7454acb37f5c20ea647f9d1",
+    "url": "/vigilantes-de-belona/static/media/noun_hearth_2563703.543b31a7.png"
+  },
+  {
+    "revision": "7d10d7a29c98ec2ad467ddc9656a9870",
+    "url": "/vigilantes-de-belona/static/media/noun_hearthbreak_2563703.7d10d7a2.png"
+  },
+  {
+    "revision": "79e2ab7e79df5f0737244cabb7c7bd26",
+    "url": "/vigilantes-de-belona/static/media/noun_skulldead_2817608.79e2ab7e.png"
+  },
+  {
+    "revision": "ddc32fe694ed423459e836d9db122373",
+    "url": "/vigilantes-de-belona/static/media/noun_skulllive_2817608.ddc32fe6.png"
+  },
+  {
+    "revision": "88f811d9aba30b4061d88cd3525c2930",
+    "url": "/vigilantes-de-belona/static/media/orco.88f811d9.jpg"
+  }
+]);
